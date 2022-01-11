e075e466852bbf977212d0da0128aeeea135d377
Valencia Caldwell <VKCaldwell@gmail.com>
2022-01-10 20:33:59 -0600

    commit tree c713569c9fcf0bfed20e13bad1d0d1fcbcd25eb6

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,14 @@
 class Student
+  attr_accessor :name, :grade, :id) 
 
+  
+   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  def initialize (:name, :grade, :id = nil) 
+    self.name = name
+    self.grade = grade 
+    self.id = id
+  end
   
 end
